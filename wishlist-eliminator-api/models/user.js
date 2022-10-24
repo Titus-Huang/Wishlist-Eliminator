bd798e0e5daf3eadd882939c832cd7564783c182
Titus Huang <titus.the.huang@gmail.com>
2022-10-24 22:26:20 +1030

    User creation updated with model file

--- wishlist-eliminator-api/models/user.js
+++ wishlist-eliminator-api/models/user.js
@@ -3,14 +3,18 @@ const db = require('../db/db');
 const User = {
     create: (username, email, passwordDigest) => {
         const sql = `
-            INSERT INTO users (username, email, password_digest)
-            VALUES ($1, $2, $3)
+            INSERT INTO users (username, email, password_digest, user_type)
+            VALUES ($1, $2, $3, 'user')
             RETURNING *
         `;
 
         return db
             .query(sql, [username, email, passwordDigest])
-            .then(dbRes => dbRes.rows[0]);
+            .then(dbRes => {
+                let userData = dbRes.rows[0];
+                delete userData.password_digest;
+                return userData;
+            });
     }
 }
 
