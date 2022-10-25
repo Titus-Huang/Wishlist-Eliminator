976784a0baf66e3700787289a48d9e99dbc1fe8e
Titus Huang <titus.the.huang@gmail.com>
2022-10-26 01:37:01 +1030

    database tips added

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE wishlist_eliminator_db;
 -- \c wishlist_eliminator_db
 
--- heroku pg:push wishlist_eliminator_db DATABASE_URL
+-- Reset & Updates database
+-- heroku pg:reset --confirm wishlist-eliminator-titus; heroku pg:push wishlist_eliminator_db DATABASE_URL
+
+-- Manually access Heroku database
+-- heroku pg:psql
 
 CREATE TYPE user_types AS ENUM ('user', 'admin');
 
