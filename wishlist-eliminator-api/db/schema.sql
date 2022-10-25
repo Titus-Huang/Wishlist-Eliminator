e3d6fd6016b0045954de6d51bdccb064a8502a10
Titus Huang <titus.the.huang@gmail.com>
2022-10-25 16:09:47 +1030

    Added wishlist table with database

--- wishlist-eliminator-api/db/schema.sql
+++ wishlist-eliminator-api/db/schema.sql
@@ -22,3 +22,28 @@ ALTER SEQUENCE users_id_seq RESTART WITH 1;
 
 -- Adding additional datapoints within table
 ALTER TABLE users ADD COLUMN steam_id TEXT;
+
+
+
+CREATE TABLE wishlists(
+    id serial primary key,
+    user_id integer,
+    added_time timestamp,
+    edited_time timestamp,
+    steam_sorted_game_ids integer[],
+    local_sorted_game_ids integer[],
+    local_sorted_game_name text[],
+    local_sorted_game_background text[],
+    local_sorted_date_added timestamp[],
+    local_sorted_release_date timestamp[],
+    local_sorted_release_date_string text[],
+    local_sorted_deck_compatibility integer[],
+    purchased boolean[]
+);
+
+-- Display total data from users
+SELECT * FROM wishlists;
+
+-- Reset Users table
+TRUNCATE TABLE wishlists;
+ALTER SEQUENCE wishlists_id_seq RESTART WITH 1;
