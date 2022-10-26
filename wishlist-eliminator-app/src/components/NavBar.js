1e93d1bfb621e4754685559d439068dd4d18e0e2
Titus Huang <titus.the.huang@gmail.com>
2022-10-26 13:38:59 +1030

    added additional routes and params for lists

--- wishlist-eliminator-app/src/components/NavBar.js
+++ wishlist-eliminator-app/src/components/NavBar.js
@@ -11,7 +11,10 @@ function NavBar(props) {
                 {!props.userData.username && <li><Link to='/users/login'>Log in</Link></li>}
                 {props.userData.username && <li><Link to='/wishlists/import'>Import Wishlist</Link></li>}
                 {props.userData.username && <li><Link to='/wishlists/create'>Create Wishlist</Link></li>}
-                {props.userData.username && <li><Link to='/wishlists/edit'>Edit Wishlist</Link></li>}
+                {props.userData.username && location.pathname.match(/(\/)(?=wishlist)\w+/gi) && <li><Link to='/wishlists/edit/1'>Edit First Wishlist</Link></li>}
+                {props.userData.username && location.pathname.match(/(\/)(?=wishlist)\w+/gi) && <li><Link to='/wishlists/view/1'>View First Wishlist</Link></li>}
+                {props.userData.username && location.pathname.match(/(\/)(?=wishlist)\w+/gi) && <li><Link to='/wishlists/edit/1'>Edit Third Wishlist</Link></li>}
+                {props.userData.username && location.pathname.match(/(\/)(?=wishlist)\w+/gi) && <li><Link to='/wishlists/view/1'>View Third Wishlist</Link></li>}
                 {props.userData.username && <li><Link to='/users/logout'>Log out</Link></li>}
             </ul>
             {console.log('hash', location.hash, '|', 'pathname', location.pathname, '|', 'search', location.search)}
