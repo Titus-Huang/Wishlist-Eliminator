f885c4691337672fa89abdbfbcb6c95e18acbed1
Titus Huang <titus.the.huang@gmail.com>
2022-10-26 14:14:36 +1030

    rough layouting of create wishlist page

--- wishlist-eliminator-app/src/components/wishlist/Create.js
+++ wishlist-eliminator-app/src/components/wishlist/Create.js
@@ -1,8 +1,15 @@
+import WishlistDisplay from "./WishlistDisplay";
 
 function Create(props) {
     return (
         <div className="WishlistCreate">
             <h2>Create Wishlist time</h2>
+            <p>the left would show the "main" wishlist, imported from Steam. While the right would show the "new" wishlist that is being created.</p>
+            <p>The top would allow the addition of wishlist name/description and so on.</p>
+            <div className="displayCreateColumns">
+                <WishlistDisplay />
+                <WishlistDisplay />
+            </div>
         </div>
     );
 }
