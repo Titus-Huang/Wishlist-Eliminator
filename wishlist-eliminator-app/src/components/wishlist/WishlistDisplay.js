e7ff8edb01d0473ba15d669455965d6fd8d08985
Titus Huang <titus.the.huang@gmail.com>
2022-10-27 16:09:31 +1030

    list data is repacked, ready to be sent back

--- wishlist-eliminator-app/src/components/wishlist/WishlistDisplay.js
+++ wishlist-eliminator-app/src/components/wishlist/WishlistDisplay.js
@@ -5,14 +5,18 @@ import './WishlistDisplay.scss';
 
 function WishlistDisplay(props) {
     const [ list, updateList ] = useState([])
-    // let list = [];
+    const [ isUpdateTime, setIsUpdateTime ] = useState(false)
+    const [ isInitialized, setIsInitialized ] = useState(false);
+    const [ isListUnpacking, setIsListUnpacking ] = useState(false);
+    const [ isListUnpacked, setIsListUnpacked ] = useState(false);
+    const [ isListRepacking, setIsListRepacking ] = useState(false);
+    const [ isListRepacked, setIsListRepacked ] = useState(false);
 
-    let isInitialized = false;
     const onInitialize = () => {
         // console.log('props', props)
         // console.log('props', Object.values(props)[1].listId)
         // console.log('props', typeof Object.values(props)[1].listId === 'undefined')
-        if (!isInitialized && typeof Object.values(props)[1].listId !== 'undefined') {
+        if (!isInitialized && !isListUnpacking && typeof Object.values(props)[1].listId !== 'undefined') {
             console.log(Object.values(props)[1].listId)
             // temp fix, because react likes to add additional data for some reason
             // updateList([])
@@ -32,17 +36,31 @@ function WishlistDisplay(props) {
             }
             console.log('Wishlist Display is initialized!');
 
-            isInitialized = true;
+            setIsInitialized(true);
+        }
+    }
+
+    const onListDataUpdated = () => {
+        // console.log(isInitialized)
+        //     console.log(isListUnpacked)
+        //     console.log(isListUnpacking)
+        if (isInitialized && isListUnpacked && !isListUnpacking && !isListRepacking && isUpdateTime) {
+            // console.log(isInitialized)
+            // console.log(isListUnpacked)
+            // console.log(isListUnpacking)
+            props.listActions.updateCurrentListData(props.type, repackList(list))
         }
     }
 
 
     const unpackList = (listData) => {
         if (listData !== null) {
-            // console.log('unpacking');
+            console.log('unpacking');
+            console.log(listData)
             const listKeys = Object.keys(listData);
             const listValues = Object.values(listData);
             if (listValues.indexOf(null) === -1) {
+                setIsListUnpacking(true);
                 let returnListArr = [];
                 for (let i = 0; i < listValues[0].length; i++) {
                     let newArrData = {}
@@ -51,9 +69,9 @@ function WishlistDisplay(props) {
                     }
                     returnListArr.push(newArrData);
                 }
-
                 updateList(returnListArr);
-
+                setIsListUnpacking(false);
+                setIsListUnpacked(true);
                 // console.log('returnListArrrrrrrrr',returnListArr);
                 // console.log('list',list);
             }
@@ -62,29 +80,34 @@ function WishlistDisplay(props) {
 
     const repackList = (listData) => {
         if (listData !== null) {
-            console.log('packing');
-            const listKeys = Object.keys(listData);
+            console.log('repacking');
+            setIsListRepacking(true);
+            const listKeys = Object.keys(listData[0]);
             const listValues = Object.values(listData);
-            if (listValues.indexOf(null) === -1) {
-                let returnListObj = [];
-                for (let i = 0; i < listValues[0].length; i++) {
-                    let newArrData = {}
-                    for (let j = 0; j < listKeys.length; j++) {
-                        newArrData[listKeys[j]] = listValues[j][i];
-                    }
-                    returnListObj.push(newArrData);
-                }
-                // updateList(returnListArr);
-                // props.listActions.
 
-                // console.log('returnListArrrrrrrrr',returnListArr);
+            console.log(listKeys);
+            console.log(listValues);
+
+            if (listKeys.indexOf(null) === -1) {
+                let returnListObj = {};
+                for (let i = 0; i < listKeys[0].length; i++) {
+                    let newArrData = []
+                    for (let j = 0; j < listValues.length; j++) {
+                        // console.log(listValues[j][listKeys[i]])
+                        newArrData[j] = listValues[j][listKeys[i]];
+                    }
+                    returnListObj[listKeys[i]] = newArrData;
+                }
+                // props.listActions.
+                // setIsListUnpacked(false);
+                console.log('returnListObj',returnListObj);
+                setIsListRepacking(false);
+                setIsListRepacked(true);
+                return returnListObj;
                 // console.log('list',list);
             }
         }
     }
-
-    
-    useEffect(onInitialize, []);
 
     const editingReferenceRender = () => {
         return (
@@ -124,6 +147,17 @@ function WishlistDisplay(props) {
             </div>
         )
     }
+
+    const moveToOtherList = () => {
+        
+    }
+
+    // useEffect(onInitialize, []);
+    useEffect(onListDataUpdated, [isUpdateTime]);
+    useEffect(onInitialize, [props.referenceListData?.list_data, props.editingListData?.list_data]);
+
+    // commands that the cards are allowed to have
+    const cardCommands = { moveToOtherList }
 
     return (
         <div className="WishlistDisplay">
