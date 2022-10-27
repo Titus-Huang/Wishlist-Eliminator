979a635763af3bb426e46936f6e682775c9fb92f
Titus Huang <titus.the.huang@gmail.com>
2022-10-27 17:39:51 +1030

    too anoyed by double debug calls

--- wishlist-eliminator-app/src/index.js
+++ wishlist-eliminator-app/src/index.js
@@ -8,13 +8,13 @@ import ErrorBoundary from './ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <BrowserRouter>
             <ErrorBoundary>
                 <App />
             </ErrorBoundary>
         </BrowserRouter>
-    </React.StrictMode>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
