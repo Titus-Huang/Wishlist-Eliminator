d888bd9ff5b6c0a0a201d1f78e04e0bdd5d56e05
Titus Huang <titus.the.huang@gmail.com>
2022-10-28 12:38:08 +1030

    some app and home css updates

--- wishlist-eliminator-app/src/components/Home.js
+++ wishlist-eliminator-app/src/components/Home.js
@@ -13,37 +13,41 @@ function Home(props) {
 
     const loggedInView = () => {
         return (
-            <div className='home-page default-content-box row'>
-                <div className="col-lg-6 col-12">
-                    <h1>Welcome to Wishlist Eliminator</h1>
+            <div className='Home col-12'>
+                <div className='home-page default-content-box row'>
+                    <div className="col-lg-6 col-12">
+                        <h1>Welcome to Wishlist Eliminator</h1>
+                    </div>
+                    <div className="col-lg-6 col-12">
+                        <p>Hi <i>{props.userData.username}</i>, hope you're enjoying your time on this site {'<3'}</p>
+                    </div>
+                    
+                    <div className="col-12 text-center footer">
+                        <p>Made with &hearts; in Adelaide</p>
+                        <p>Created by <a href="https://github.com/Titus-Huang">Titus Huang</a></p>
+                        <p>&copy; 2022, General Assembly SEiR 61, MIT Licence</p>
+                    </div>
+                    {/* <p>Counter: {counter}</p>
+                    <button onClick={increaseCounter}>Count</button> */}
                 </div>
-                <div className="col-lg-6 col-12">
-                    <p>Hi <i>{props.userData.username}</i>, hope you're enjoying your time on this site {'<3'}</p>
-                </div>
-                
-                <div className="col-12 text-center footer">
-                    <p>Made with &hearts; in Adelaide</p>
-                    <p>Created by <a href="https://github.com/Titus-Huang">Titus Huang</a></p>
-                    <p>&copy; 2022, General Assembly SEiR 61, MIT Licence</p>
-                </div>
-                {/* <p>Counter: {counter}</p>
-                <button onClick={increaseCounter}>Count</button> */}
             </div>
         );
     }
     
     const loggedOutView = () => {
         return (
-            <div className='landing-page default-content-box row'>
-                <div className="landing-page-intro col-lg-8 col-sm-12">
-                    <h1>Welcome to Wishlist Eliminator</h1>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis perspiciatis soluta deleniti quae dignissimos recusandae, eligendi odit officia, aut esse, at quam maiores? Autem nulla officia perferendis temporibus, reprehenderit qui!</p>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus id odit voluptatem beatae repellat quos nihil iste aut quo. Fuga vitae culpa iusto ipsam molestiae recusandae? Adipisci dolor necessitatibus reprehenderit! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro facilis sit quos facere exercitationem ut quidem voluptate distinctio natus deserunt laborum alias, error voluptatibus delectus, repudiandae soluta reiciendis ad temporibus.</p>
-                </div>
-                <div className="landing-page-user-links col-lg-4 col-sm12">
-                    <div className="btn-group btn-group-lg" role="user-onboarding" aria-label="Sign up and Log in buttons">
-                        <button className='btn btn-primary' onClick={() => navigate('/users/sign-up')}>Sign up</button>
-                        <button className='btn btn-primary' onClick={() => navigate('/users/login')}>Log in</button>
+            <div className='Home col-12 home-buffer'>
+                <div className='landing-page default-content-box row'>
+                    <div className="landing-page-intro col-lg-8 col-sm-12">
+                        <h1>Welcome to Wishlist Eliminator</h1>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis perspiciatis soluta deleniti quae dignissimos recusandae, eligendi odit officia, aut esse, at quam maiores? Autem nulla officia perferendis temporibus, reprehenderit qui!</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus id odit voluptatem beatae repellat quos nihil iste aut quo. Fuga vitae culpa iusto ipsam molestiae recusandae? Adipisci dolor necessitatibus reprehenderit! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro facilis sit quos facere exercitationem ut quidem voluptate distinctio natus deserunt laborum alias, error voluptatibus delectus, repudiandae soluta reiciendis ad temporibus.</p>
+                    </div>
+                    <div className="landing-page-user-links col-lg-4 col-sm12">
+                        <div className="btn-group btn-group-lg" role="user-onboarding" aria-label="Sign up and Log in buttons">
+                            <button className='btn btn-primary' onClick={() => navigate('/users/sign-up')}>Sign up</button>
+                            <button className='btn btn-primary' onClick={() => navigate('/users/login')}>Log in</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -51,10 +55,10 @@ function Home(props) {
     };
 
     return (
-        <div className='Home col-12'>
+        <>
             {props.userData.username && loggedInView()}
             {!props.userData.username && loggedOutView()}
-        </div>
+        </>
     )
 }
 
