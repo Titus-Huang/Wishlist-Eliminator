3f0373a4d8dce65cf4a95c039e8cd9cb3936680d
Titus Huang <titus.the.huang@gmail.com>
2022-10-28 12:05:12 +1030

    log out page functional

--- wishlist-eliminator-app/src/components/users/Logout.js
+++ wishlist-eliminator-app/src/components/users/Logout.js
@@ -1,8 +1,24 @@
-
+import { useNavigate } from "react-router-dom";
 
 function Logout() {
+    const navigate = useNavigate();
+
+    fetch('/api/sessions', {
+        method: 'DELETE'
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            navigate('/');
+            window.location.reload();
+        })
+
     return (
-        <h3>Logout render</h3>
+        <div className="default-content-box row">
+            <div className="col-12 text-center">
+                <h3>Logging out...</h3>
+            </div>
+        </div>
     )
 }
 
